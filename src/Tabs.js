7de7f49f2cf7de4b4d418364d9b564853918fe6e
ken_ellingsen <ken.ellingsen@gmail.com>
2024-08-28 14:27:40 -0500

    Initial commit

---
+++ src/Tabs.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const Tabs = ({ services }) => { // {services} destructures 'services' from the prop
+
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  return (
+    <section className="tabs">
+      <div>
+        {services.map((service, index) => (
+          <h3 
+            className="tab-title"
+            key={index}
+          >
+            {service.name}
+          </h3>
+        ))}
+      </div>
+      <div className="tab-content">
+        <p>{services[activeTabIndex].description}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Tabs;
